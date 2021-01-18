98ad65ba3750848429e2c5723c0f7687c1820bb8
pushpendrakumar <pkc3766@gmail.com>
2021-01-18 11:17:15 +0530

    added ejs dependency

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // jshint esversion:6
 const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 const app = express();
+app.set('view engine','ejs');
+app.use(bodyParser,urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.render("home");
+});
 
 app.listen(3000,function(){
     console.log("server started at port 3000");
