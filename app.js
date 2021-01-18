b263c4b3c842494a146146c4d28dfe6649af6d71
pushpendrakumar <pkc3766@gmail.com>
2021-01-18 14:32:28 +0530

    added utilityfunctions.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const utilityFunctions = require(__dirname+"/utilityFunctions.js");
 const app = express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+//console.log(utilityFunctions);
+//can call utiltiy functions using utilityFunctions.getDate() etc
 
 app.get("/",function(req,res){
     res.render("home");
---
+++ utilityFunctions.js
@@ -0,0 +1,10 @@
+module.exports.getDate=function (){
+
+};
+
+module.exports.getDay=function (){
+
+};
+
+
+
