90a32a3e543027354dbc15c6453676c1a3b7826d
pushpendrakumar <pkc3766@gmail.com>
2021-01-20 15:03:24 +0530

    added routing in cs-fundamentals added non-function-routes handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 // const utilityFunctions = require(__dirname+"/scripts/utilityFunctions.js");
 //const homePageScript = require(__dirname+"/scripts/homePageScript");
 const app = express();
@@ -13,12 +14,26 @@ app.use(express.static("public"));
 //console.log(utilityFunctions);
 //can call utiltiy functions using utilityFunctions.getDate() etc
 
+
 app.get("/",function(req,res){
-    
-    res.render("html/home.html");
+    res.render("pages/home");
 });
 
-// app.get("/")
+app.get("/cs-fundamentals/:subject",function(req,res){
+    const subject = _.kebabCase(req.params.subject);
+    console.log(subject);
+    const path='pages/cs-fundamentals/post';
+    //res.send();
+    res.render(path,{subject:subject});
+});
+
+
+//handles non routable paths
+app.get('*', function(req, res) {  
+    res.render('pages/error');
+});
+
+
 
 app.listen(3000,function(){
     console.log("server started at port 3000");
