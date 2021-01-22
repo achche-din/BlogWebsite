95c74132433cab8aebc018a0925e049699456d68
pushpendrakumar <pkc3766@gmail.com>
2021-01-23 01:10:35 +0530

    added database to compose article

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+const session = require('express-session'); 
+
 const _ = require("lodash");
+const mongoose = require('mongoose');
+const { body,validationResult } = require('express-validator');
 // const utilityFunctions = require(__dirname+"/scripts/utilityFunctions.js");
 //const homePageScript = require(__dirname+"/scripts/homePageScript");
+const composedb = require(__dirname+'/composedb.js');
 const app = express();
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser('secret'))
+app.use(session({cookie: {maxAge: null}}))
+
+//flash message middleware
+app.use((req, res, next)=>{
+    res.locals.message = req.session.message
+    delete req.session.message
+    next()
+  })
+
+// app.use(expressValidator());
 app.use(express.static("public"));
 
 //console.log(utilityFunctions);
 //can call utiltiy functions using utilityFunctions.getDate() etc
-
+composedb.connection();
+const Post = composedb.createSchema();
 
 app.get("/",function(req,res){
     res.render("pages/home");
@@ -56,6 +74,71 @@ app.get("/contact",function(req,res){
     const path='pages/contact';
     //res.send();
     res.render(path);
+});
+
+app.post("/compose",[body('name', 'name should not be empty').trim().isLength({ min: 1 }),
+    body('companyName', 'company name should not be empty').trim().isLength({ min: 1 }),
+    body('email', 'Invalid email').trim().isEmail(),
+    body('content', 'Article must have characters > 50').trim().isLength({ min: 1 })],
+    function(req,res){
+    //need to validate form also
+    let errors = validationResult(req);
+    // console.log(typeof errors);
+    // console.log(errors);
+    // console.log("array");
+    // console.log(errors.array().length);
+    // console.log(errors.array()[0].msg);
+    // console.log(errors.array());
+    // console.log("mapped");
+    // console.log(errors.mapped());
+    //res.send("");
+    let errorArray=errors.array();
+    if(errorArray.length>0)
+    {
+        // let d='';
+        // errorArray.forEach(function(error){
+        //     d = d+error.msg+"<br>";
+        // });
+        req.session.message = {
+            type: 'danger',
+            intro: 'Empty fields! ',
+            message: 'Please insert valid information.'
+          }
+        res.redirect('compose');
+        // console.log(errorArray.length);
+        // console.log(errorArray[0].msg);
+        // res.render('pages/compose',{errors: errorArray});
+        // return res.status(422).json({ errors: errors.array() });
+    }
+    else{
+        req.session.message = {
+            type: 'success',
+            intro: 'Submitted',
+            message: 'your article has been submitted successfully'
+          }
+            const title=req.body.title;
+            const companyName= req.body.companyName;
+            const name=req.body.name;
+            const email=req.body.email;
+            const content=req.body.content;
+            const post = new Post({
+                title: title,
+                companyName: companyName,
+                name:name,
+                email: email,
+                content: content
+            });
+            post.save(function(err){
+                if (!err){
+                    //make a toast that your post has been sent. Our team will look into it.
+                    res.redirect("compose");
+                }
+                else{
+                    res.send("error occured");
+                }
+            });
+        }
+    
 });
 
 //handles non routable paths
---
+++ composedb.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+module.exports.connection=function(){
+    //connect with database
+    mongoose.connect('mongodb://localhost/composedb',{useNewUrlParser:true},{ useUnifiedTopology: true });
+    mongoose.connection.once('open',function(){
+        console.log('connection has been made');
+    }).on('error',function(error){
+        console.log('error is'+error);
+    });
+};
+
+module.exports.createSchema=function(){
+    //connection();
+    const composedb= mongoose.Schema({
+        title:String,
+        companyName:String,
+        name:String,
+        email:String,
+        content:String
+    });
+    return mongoose.model("Post",composedb);
+};
+
+
+
